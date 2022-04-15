ff2fe3b279f6b45488042ba1f18998c06144533a
mo.ismat <mo.ismat@auto1.com>
2022-04-15 15:35:38 +0200

    init commit

---
+++ src/test/java/com/moismat/springrestproject/SpringRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moismat.springrestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
