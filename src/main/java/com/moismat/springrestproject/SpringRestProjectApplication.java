1018d8d35c676e075a9b586e162bb8d5cd30f85d
mo.ismat <mo.ismat@auto1.com>
2022-04-16 17:14:56 +0200

    03 use @configurationproperties

--- src/main/java/com/moismat/springrestproject/SpringRestProjectApplication.java
+++ src/main/java/com/moismat/springrestproject/SpringRestProjectApplication.java
@@ -3,6 +3,8 @@ package com.moismat.springrestproject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringRestProjectApplication {
 
 	public static void main(String[] args) {
@@ -48,20 +51,20 @@ class DataLoader {
 @RestController
 @RequestMapping("/greeting")
 class GreetingController {
-	@Value("${greeting-name: Mirage}")
-	private String name;
+	private final Greeting greeting;
 
-	@Value("${greeting-coffee: ${greeting-name} is drinking Café Ganador}")
-	private String coffee;
+	public GreetingController(Greeting greeting) {
+		this.greeting = greeting;
+	}
 
 	@GetMapping
 	String getGreeting() {
-		return name;
+		return greeting.getName();
 	}
 
 	@GetMapping("/coffee")
 	String getNameAndCoffee() {
-		return coffee;
+		return greeting.getCoffee();
 	}
 }
 
@@ -99,6 +102,28 @@ class CoffeeController {
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
 		coffeeRepository.deleteById(id);
+	}
+}
+
+@ConfigurationProperties(prefix = "greeting")
+class Greeting {
+	private String name;
+	private String coffee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
 	}
 }
 
