bc4ae370985f489e840558c326fcf64403ce039b
mo.ismat <mo.ismat@auto1.com>
2022-04-16 15:50:07 +0200

    03 use application.properties with @value

--- src/main/java/com/moismat/springrestproject/SpringRestProjectApplication.java
+++ src/main/java/com/moismat/springrestproject/SpringRestProjectApplication.java
@@ -1,5 +1,6 @@
 package com.moismat.springrestproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
@@ -41,6 +42,26 @@ class DataLoader {
 				new Coffee("Café Lareño"),
 				new Coffee("Café Três Pontas")
 		));
+	}
+}
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+	@Value("${greeting-name: Mirage}")
+	private String name;
+
+	@Value("${greeting-coffee: ${greeting-name} is drinking Café Ganador}")
+	private String coffee;
+
+	@GetMapping
+	String getGreeting() {
+		return name;
+	}
+
+	@GetMapping("/coffee")
+	String getNameAndCoffee() {
+		return coffee;
 	}
 }
 
